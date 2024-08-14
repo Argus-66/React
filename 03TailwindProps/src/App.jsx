c418172f755943f8f5d1d013f62649f4cf71cdfa
Ayush H <jqghebri@gmail.com>
2024-08-15 01:46:16 +0530

    creation

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -8,7 +8,12 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+
+      
+
+
+
     </>
   )
 }
