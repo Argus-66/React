94b3bed1ff790a688a99a40c1da7d36cb59cd100
Ayush H <jqghebri@gmail.com>
2024-08-14 17:44:05 +0530

    creation

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -5,9 +5,36 @@ import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(15)
+
+
+
+  //let counter = 15
+
+  const addValue = () =>{
+    counter = counter + 1;
+    setCounter(counter)
+    console.log("value added!", counter)
+  }
+
+  const removeValue = () =>{
+    counter = counter - 1;
+    setCounter(counter)
+    console.log("value removed!", counter)
+  }
+
   return (
     <>
       <h1>React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick = {addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick= {removeValue}
+      >Remove value</button>
     </>
   )
 }
