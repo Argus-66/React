b93de4498ba7b29a5fd9bc662975bc8916d6fd3a
Ayush H <jqghebri@gmail.com>
2024-08-14 23:55:01 +0530

    creation

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,42 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
-  let [counter, setCounter] = useState(15)
-
-
+  let [counter, setCounter] = useState(15);
 
   //let counter = 15
 
-  const addValue = () =>{
-    counter = counter + 1;
-    setCounter(counter)
-    console.log("value added!", counter)
-  }
+  const addValue = () => {
+    if (counter < 20) {
+      counter = counter + 1;
+      setCounter(counter);
+      console.log("value added!", counter);
+    }
+  };
 
-  const removeValue = () =>{
-    counter = counter - 1;
-    setCounter(counter)
-    console.log("value removed!", counter)
-  }
+  const removeValue = () => {
+    if (counter > 0) {
+      counter = counter - 1;
+      setCounter(counter);
+      console.log("value removed!", counter);
+    }
+  };
 
   return (
     <>
       <h1>React</h1>
       <h2>Counter Value: {counter}</h2>
 
-      <button
-      onClick = {addValue}
-      >Add value</button>
+      <button onClick={addValue}>Add value</button>
       <br />
-      <button
-      onClick= {removeValue}
-      >Remove value</button>
+      <button onClick={removeValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
